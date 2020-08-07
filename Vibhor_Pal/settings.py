8ca2b88bdbe27e31340d69418756bd54ac96bdb4
Vibhor Pal <43597450+vibhorpal4@users.noreply.github.com>
2020-08-07 14:29:29 +0530

    Update settings.py

--- Vibhor_Pal/settings.py
+++ Vibhor_Pal/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,6 +126,3 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
