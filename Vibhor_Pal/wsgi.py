67db009ff4c4cc39ee8c69c663b2983b7f743b55
Vibhor Pal <43597450+vibhorpal4@users.noreply.github.com>
2020-08-07 14:52:10 +0530

    Update wsgi.py

--- Vibhor_Pal/wsgi.py
+++ Vibhor_Pal/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vibhor_Pal.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Vibhor_Pal.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
